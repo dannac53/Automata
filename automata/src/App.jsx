f80a4d13df3b1b5236e30c89375dc72fa8cb42d5
RobertoBula <mozrobertocarlos@gmail.com>
2022-09-16 22:45:45 -0500

    add table

--- automata/src/App.jsx
+++ automata/src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Formulario from "./formulario/Formulario";
-
+import Home from "./home/Home"
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route index element={<Home/>}/>
         <Route path="formulario" element={<Formulario />} />
       </Routes>
     </BrowserRouter>
