d14a2ba4a1206a0b4a93f04570d38dc7f5a255ac
RobertoBula <mozrobertocarlos@gmail.com>
2022-09-26 16:36:11 -0500

    fix table

--- automata/src/home/Home.jsx
+++ automata/src/home/Home.jsx
@@ -1,11 +1,13 @@
 import "./Home.css";
 import { convertExcelToArray } from "../utils/excel";
-import { validate } from "../utils/validate";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   const getFile = (event) => {
     const file = event.target.files[0];
-    convertExcelToArray(file, (result) => validate(result));
+    convertExcelToArray(file, (result) => {
+      console.log(result);
+    });
   };
   const selectFile = () => {
     document.getElementById("file").click();
@@ -13,61 +15,66 @@ const Home = () => {
   return (
     <div className="all row">
       <header className="col-12">Tabla Estudiantes</header>
-      <div className="card col-12">
+      <div className="card col-12" id="tarjeta">
         <div className="seccion">
-          <input className="search col-6" type="text" placeholder="Search" />
+          <input
+            className="search col-6"
+            type="text"
+            name=""
+            id=""
+            placeholder="Search"
+          />
           <i class="bx bx-search bx-sm" id="icon"></i>
-          <button className="btn-primary btn-lg" id="adduser">
-            Add user +
-          </button>
+          <Link to ="/formulario" className="btn-lg" id="adduser">
+          Add student
+            <i class="bx bx-user-plus bx-sm" id="icon2"></i>  
+          </Link>
         </div>
-        <header className="title"> Suppliers</header>
-        <button id="export" onClick={() => selectFile()}>
-          <i className="bx bx-export bx-sm"></i>Importar
-        </button>
-        <button id="import">
-          <i className="bx bx-import bx-sm"></i>Exportar
-        </button>
-        <input
-          type="file"
-          accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
-          hidden
-          id="file"
-          onChange={(e) => getFile(e)}
-        />
+        <div className="btn">
+          <button id="export" onClick={() => selectFile()}>
+            <i class="bx bx-export bx-sm"></i> Export
+          </button>
+          <button id="import">
+            <i class="bx bx-import bx-sm"></i> Import
+          </button>
+          <button id="excel">
+            <i class="bx bxs-file-doc bx-sm"></i> Excel
+          </button>
+          <input
+            type="file"
+            accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
+            hidden
+            id="file"
+            onChange={(e) => getFile(e)}
+          />
+        </div>
         <div className="tabla">
           <table className="table">
             <thead>
               <tr>
-                <th></th>
-                <th>First</th>
-                <th>Last</th>
-                <th>Handle</th>
+                <th scope="col">Codigo</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Celular</th>
+                <th scope="col">Correo</th>
+                <th scope="col">Acciones</th>
               </tr>
             </thead>
             <tbody>
               <tr>
-                <th>
-                  <input type="checkbox" />
-                </th>
+                <th scope="col"></th>
                 <td>Mark</td>
                 <td>Otto</td>
                 <td>@mdo</td>
-              </tr>
-              <tr>
-                <th>
-                  <input type="checkbox" />
-                </th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-              </tr>
-              <tr>
-                <th>
-                  <input type="checkbox" />
-                </th>
-                <td>Larry the Bird</td>
-                <td>@twitter</td>
+                <td>
+                  <i
+                    onClick={() => setDataForm(usuario)}
+                    className="bx bxs-edit bx-sm"
+                  ></i>
+                  <i
+                    onClick={() => deleteById(usuario.id)}
+                    className="bx bxs-trash bx-sm"
+                  ></i>
+                </td>
               </tr>
             </tbody>
           </table>
