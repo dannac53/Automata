8ef3958708543f49898047c5b8e62820f475e8ec
RobertoBula <mozrobertocarlos@gmail.com>
2022-09-26 22:36:59 -0500

    fix button add

--- automata/src/home/Home.jsx
+++ automata/src/home/Home.jsx
@@ -3,8 +3,43 @@ import { convertExcelToArray } from "../utils/excel";
 import { Link } from "react-router-dom";
 import { validate } from "../utils/validate";
 import { firestoreService } from "../firestore_service";
+import { useState } from "react";
+import { useEffect } from "react";
+import { alert } from "../alert";
 
 const Home = () => {
+  const [copy, setCopy] = useState([]);
+  const [students, setStudents] = useState([]);
+  const getStudents = () => {
+    firestoreService.getStudents().then((response) => {
+      const data = [];
+      response.docs.map((doc) => data.push({ id: doc.id, ...doc.data() }));
+      setStudents(data);
+      setCopy(data);
+    });
+  };
+
+  const deleteById = (id) => {
+    alert.confirmDelete().then((result) => {
+      if (result.isConfirmed) {
+        firestoreService.deleteById(id).then(() => {
+          const rows = students.filter((student) => student.id !== id);
+          setStudents(rows);
+          alert.success("Estudiante eliminado con éxito");
+        });
+      }
+    });
+  };
+  const filterStudent = (name) => {
+    console.log(name);
+    if (name.length === 0) setStudents(copy);
+    else {
+      const expresion = new RegExp(`${name}.*`, "i");
+      const result = students.filter((student) => expresion.test(student.name));
+      setStudents(result);
+    }
+  };
+
   const getFile = (event) => {
     const file = event.target.files[0];
     convertExcelToArray(file, (result) => {
@@ -14,20 +49,20 @@ const Home = () => {
   const selectFile = () => {
     document.getElementById("file").click();
   };
+  useEffect(() => getStudents(), []);
   return (
     <div className="all row">
       <header className="col-12">Tabla Estudiantes</header>
       <div className="card col-12" id="tarjeta">
         <div className="seccion">
           <input
+            onChange={(event) => filterStudent(event.target.value)}
             className="search col-6"
             type="text"
-            name=""
-            id=""
             placeholder="Search"
           />
           <i class="bx bx-search bx-sm" id="icon"></i>
-          <Link to="/formulario" className="btn-lg" id="adduser">
+          <Link to="/formulario" className="addstudent">
             Add student
             <i class="bx bx-user-plus bx-sm" id="icon2"></i>
           </Link>
@@ -62,22 +97,26 @@ const Home = () => {
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <th scope="col"></th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-                <td>
-                  <i
-                    onClick={() => setDataForm(usuario)}
-                    className="bx bxs-edit bx-sm"
-                  ></i>
-                  <i
-                    onClick={() => deleteById(usuario.id)}
-                    className="bx bxs-trash bx-sm"
-                  ></i>
-                </td>
-              </tr>
+              {students.map((student) => {
+                return (
+                  <tr>
+                    <th>{student.code}</th>
+                    <td>{student.name}</td>
+                    <td>{student.phone}</td>
+                    <td>{student.email}</td>
+                    <td>
+                      <i
+                        onClick={() => setDataForm(usuario)}
+                        className="bx bxs-edit bx-sm"
+                      ></i>
+                      <i
+                        onClick={() => deleteById(student.id)}
+                        className="bx bxs-trash bx-sm"
+                      ></i>
+                    </td>
+                  </tr>
+                );
+              })}
             </tbody>
           </table>
         </div>
